ef27d36e024ca0039d92a66cb4fcc3ad4eff0296
Garry <gazm81@yahoo.com>
2019-07-31 08:38:23 +1000

    latest regex for jsdoc

--- tools/regexExample.ps1
+++ tools/regexExample.ps1
@@ -1,25 +1,27 @@
 ## JSDOC Regex 
 
+$fileName = '/Users/garryhughes/GIT/my-actions/src/pso.test.gh/exportedImported/exportedImported.js'
+
 # header and description
 
 $patternHeader = '(?smi)\/\*\*\n(\* .*\n)+(\*\/)'
-$patternDescription = "\/\*\*(\n(\* )[^@].*)*"
-
-$fileName = '/Users/garryhughes/GIT/my-actions/src/pso.test.gh/exportedImported/exportedImported.js'
-$fileContent = Get-Content $fileName -Raw
-$jsdocBody = $fileContent -replace '(?smi)\/\*\*\n(\* .*\n)+(\*\/)',''
-$jsdocHeader = $fileContent | Select-String '(?smi)\/\*\*\n(\* .*\n)+(\*\/)' -AllMatches | %{ $_.Matches } | %{ $_.Groups[1] } | %{ $_.Value }
-$jsDocDescription = $fileContent | Select-String -Pattern "(\/\*\*\n)(\* [^@]*\n)*" -AllMatches | %{ $_.Matches } | %{ $_.Groups[2] } | %{ $_.Value }
-$jsDocDescription = $jsDocDescription.split([System.Environment]::NewLine) | % { $_ -replace "^\* ","" }
-
-# jsdoc comments
-
+$patternDescription = "(\/\*\*\n)(\* [^@]*\n)*"
+$patternBody = "(?smi)^function .*\n(.*\n)*"
 $patternInputs =  "\* @(?<jsdoctype>param) (?<type>[^}]*}) (?<name>\w+) - (?<description>[^\n]*)"
 $patternReturn =  "\* @(?<jsdoctype>return) (?<type>{[^}]*})"
 $patternOther = "\* @(?<jsdoctype>\w+) (?<description>[^{]*)"
 
+$fileContent = Get-Content $fileName -Raw
+$jsdocBody = ($fileContent | Select-String -Pattern $patternBody |  % { $_.Matches.value }).split([System.Environment]::NewLine) 
+$jsdocBody = ($jsdocBody | Select-Object -Skip 1 | % { $_ -replace "^\t","" }) -join [System.Environment]::NewLine
+$jsdocHeader = $fileContent | Select-String $patternHeader -AllMatches | % { $_.Matches } | %{ $_.Groups[1] } | %{ $_.Value }
+$jsDocDescription = $fileContent | Select-String -Pattern $patternDescription -AllMatches | %{ $_.Matches } | %{ $_.Groups[2] } | %{ $_.Value }
+$jsDocDescription = $jsDocDescription.split([System.Environment]::NewLine) | % { $_ -replace "^\* ","" }
+
+# jsdoc comments
+
 $jsdocComments = @()
-$fileContent.Split([System.Environment]::NewLine) | Select-String -Pattern $patternInputs, $patternReturn , $patternOther |
+$jsdocHeader.Split([System.Environment]::NewLine) | Select-String -Pattern $patternInputs, $patternReturn , $patternOther |
     Foreach-Object {
         $jsdocComments += [PSCustomObject] @{
             jsdoctype = $_.Matches[0].Groups['jsdoctype'].Value
@@ -28,4 +30,10 @@ $fileContent.Split([System.Environment]::NewLine) | Select-String -Pattern $patt
             description = $_.Matches[0].Groups['description'].Value
         }
     }
-$jsdocComments | Format-Table
+
+Write-Host "##################"
+$jsdocHeader
+Write-Host "##################"
+$jsdocBody
+Write-Host "##################"
+$jsdocComments
