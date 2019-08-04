8e6870b45f97df6b5199f491a51cc97c4e454f81
Garry <gazm81@yahoo.com>
2019-08-04 19:45:12 +1000

    tests update

--- test.ps1
+++ test.ps1
@@ -7,3 +7,13 @@ gci $Env:AGENT_BUILDDIRECTORY
 Write-Host "BUILD_SOURCESDIRECTORY contents:"
 gci $Env:BUILD_SOURCESDIRECTORY
 Write-Host "Over and out."
+
+
+"current location: $(Get-Location)"
+"script root: $PSScriptRoot"
+"retrieve available modules"
+$modules = Get-Module -list
+if ($modules.Name -notcontains 'pester') {
+    Install-Module -Name Pester -Force -SkipPublisherCheck
+}
+Invoke-Pester -Script "./tests/" -OutputFile "./Test-Pester.XML" -OutputFormat 'NUnitXML'
