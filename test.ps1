676af4fa72dafc44350375e77da2a6a4b34a6186
Garry <gazm81@yahoo.com>
2019-08-04 15:10:29 +1000

    test updates

---
+++ test.ps1
@@ -0,0 +1,9 @@
+Write-Host "Hello World from $Env:AGENT_NAME."
+Write-Host "My ID is $Env:AGENT_ID."
+Write-Host "AGENT_WORKFOLDER contents:"
+gci $Env:AGENT_WORKFOLDER
+Write-Host "AGENT_BUILDDIRECTORY contents:"
+gci $Env:AGENT_BUILDDIRECTORY
+Write-Host "BUILD_SOURCESDIRECTORY contents:"
+gci $Env:BUILD_SOURCESDIRECTORY
+Write-Host "Over and out."
