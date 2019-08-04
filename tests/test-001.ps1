62dd7c2ab6ee068d8dc90036fa60a18a32d4823d
Garry <gazm81@yahoo.com>
2019-08-05 09:58:49 +1000

    test updates

--- tests/test-001.ps1
+++ tests/test-001.ps1
@@ -1,21 +1,21 @@
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-Describe -Tags "Export VRO IDE" {
+Describe "Export VRO IDE" {
     Mock Get-VroAction {return $vroActionHeaders}
     Mock Export-VroAction {return null}
     #Mock Build {} -Verifiable -ParameterFilter {$version -eq 1.2}
     BeforeAll {
         ## Code in here
-        Import-Module ./src/vroide.psm1 -Force
+        Import-Module (Get-Location | Join-Path -ChildPath "src" -AdditionalChildPath "vroide.psm1" ) -Force
         $vROConnection = "mocked endpoint"
         $TempDir = [System.Guid]::NewGuid().ToString()
         $vroIdeFolder = New-Item -Type Directory -Name $TempDir -path $env:TMPDIR
         $vroIdeFolder
-        $vroActionHeaders = get-content -Raw (Get-Location | Join-Path -ChildPath test -AdditionalChildPath "data" | Join-Path -ChildPath  "vroActionHeaders.json")
+        $vroActionHeaders = get-content -Raw (Get-Location | Join-Path -ChildPath "tests" -AdditionalChildPath "data" | Join-Path -ChildPath  "vroActionHeaders.json")
         $vroActionHeaders | ConvertTo-Json | set-content ($vroIdeFolder | Join-Path -ChildPath "vroActionHeaders.json")
     }
 
     It "Exports VRO Environment" {
-        Export-VroIde -vroIdeFolder $vroIdeFolder -Debug | Should be 2
+        Export-VroIde -vroIdeFolder $vroIdeFolder -Debug | Should be null
     }
 }
